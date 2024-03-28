467194b9e0e7cca38b9d347fd0ff5c4519c00e70
Abdulaziz780 <akhusanov410@gmail.com>
2024-03-28 13:42:14 +0500

    easy

---
+++ main.js
@@ -0,0 +1,10 @@
+let massiv = confirm("Savatga mahsulot kiriting")
+let arr = ['Shoftoli', 'olma', 'juhori', 'non']
+
+if (massiv === true) {
+    arr.pop(arr)
+    console.log(arr);
+}
+else {
+ console.log(arr);
+}
